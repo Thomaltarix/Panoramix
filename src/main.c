019c3930cf38002b5818d53416bd653a4e79e88a
Thomaltarix <thomas.boue@epitech.eu>
2024-04-29 08:42:30 +0200

    :children_crossing: Add a help display

--- src/main.c
+++ src/main.c
@@ -6,6 +6,16 @@
 */
 
 int main(void)
+static void display_help(void)
 {
+    write(1, "USAGE\n", 6);
+    write(1, "\t./panoramix n1 n2 n3 n4\n", 26);
+    write(1, "DESCRIPTION\n", 13);
+    write(1, "\tn1\tnumber of villagers\n", 25);
+    write(1, "\tn2\tsize of the pot\n", 21);
+    write(1, "\tn3\tnumber of fights\n", 22);
+    write(1, "\tn4\tnumber of refills\n", 23);
+}
+
     return 0;
 }
