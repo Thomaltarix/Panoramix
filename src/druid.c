e5fdbdbdbda083416fa2c483a67cde797824e10f
Thomaltarix <thomas.boue@epitech.eu>
2024-04-29 19:46:50 +0200

    :art: Improve the quality of the druid code

--- src/druid.c
+++ src/druid.c
@@ -7,20 +7,25 @@
 
 #include "config.h"
 
+static void refill(thread_data_t *thread_data)
+{
+    sem_wait(&thread_data->sync_data->semaphore);
+    printf("Druid: Ah! Yes, yes, I'm awake! Working on it! "
+    "Beware I can only make %ld more refills after this one.\n",
+    (thread_data->config->refill_nb) - 1);
+    thread_data->config->refill_nb--;
+    thread_data->config->current_pot_size = thread_data->config->pot_size;
+    pthread_mutex_unlock(&thread_data->sync_data->mutex);
+}
+
 void *druid(void *data)
 {
     thread_data_t *thread_data = (thread_data_t *)data;
 
     printf("Druid: I'm ready... but sleepy...\n");
-    while (thread_data->config->refill_nb > 0 && thread_data->config->villagers_nb > 0) {
-        sem_wait(&thread_data->sync_data->semaphore);
-        printf("Druid: Ah! Yes, yes, I'm awake! Working on it!"
-        "Beware I can only make %ld more refills after this one.\n",
-        (thread_data->config->refill_nb) - 1);
-        thread_data->config->refill_nb--;
-        thread_data->config->current_pot_size = thread_data->config->pot_size;
-        pthread_mutex_unlock(&thread_data->sync_data->mutex);
-    }
+    while (thread_data->config->refill_nb > 0 &&
+        thread_data->config->villagers_nb > 0)
+        refill(thread_data);
     printf("Druid: I'm out of viscum. I'm going back to... zZz\n");
     return NULL;
 }
